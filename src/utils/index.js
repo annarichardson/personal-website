9337657646e7e7bdbe09df5661a6c5ed3e74f6e4
annarichardson <anna.richardson@tufts.edu>
2018-05-06 23:17:26 -0400

    fix menu highlighting

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,16 +3,8 @@
  * Written by: Anna Richardson
  */
 
-export const documentHeight = () => {
-  return Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight,
-  );
-};
-
-export const scrollHeight = () => {
-  return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+export const middleScreenLocation = () => {
+  return (Math.max(document.documentElement.clientHeight, window.innerHeight || 0) / 2) + window.scrollY;
 };
 
 export const DESKTOP_WIDTH = 992;
@@ -20,8 +12,7 @@ export const TABLET_WIDTH = 768;
 export const PHONE_WIDTH = 400;
 
 export default {
-  documentHeight,
-  scrollHeight,
+  middleScreenLocation,
   DESKTOP_WIDTH,
   TABLET_WIDTH,
   PHONE_WIDTH,
